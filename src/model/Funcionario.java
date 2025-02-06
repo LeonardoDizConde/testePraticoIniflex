818d26b9567b171f14301238241e31c40195fb98
Leonidas Conde <leonardodizconde@gmail.com>
2025-02-06 12:50:18 -0300

    req-3.1

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Funcionario {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Funcionario extends Pessoa{
+    private static final DateTimeFormatter timeParser = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+    private BigDecimal salario;
+    private String funcao;
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public static Funcionario convertFromTableItem(String[] tableItem) {
+        String nome = tableItem[0];
+        LocalDate dataNascimento = LocalDate.parse(tableItem[1], timeParser);
+        BigDecimal salario = BigDecimal.valueOf(Double.parseDouble(tableItem[2]));
+        String funcao = tableItem[3];
+        return new Funcionario(nome, dataNascimento, salario, funcao);
+    }
 }
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -6,7 +6,7 @@ public class Pessoa {
     private String nome;
     private LocalDate dataNascimento;
 
-    Pessoa(String nome, LocalDate dataNascimento) {
+    public Pessoa(String nome, LocalDate dataNascimento) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
     }
